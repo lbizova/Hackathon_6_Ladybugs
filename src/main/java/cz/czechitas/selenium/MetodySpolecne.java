08d9a493fda89bf2d038d8ac9631032416ad3d8b
Lucka Bízová <bizova.lucie@gmail.com>
2021-11-20 13:17:33 +0100

    2 testy hotové (log in, log out), 3. rozpracovaný (kontrola odkazů)

--- src/main/java/cz/czechitas/selenium/MetodySpolecne.java
+++ src/main/java/cz/czechitas/selenium/MetodySpolecne.java
@@ -16,20 +16,8 @@ public class MetodySpolecne {
         prohlizec.findElement(By.xpath("//*[text()=\"" + text + "\"]")).click();
     }
 
-//    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHref(WebDriver prohlizec, String adresa) {
-//        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\"]")).click();
-//    }
-//
-//    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHrefACastiClass(WebDriver prohlizec, String adresa, String classCast) {
-//        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\" and contains (@class, '" + classCast + "')]")).click();
-//    }
-//
     public static void vyplneniPoleSId(WebDriver prohlizec, String id, String textNaVyplneniPole) {
         prohlizec.findElement(By.id(id)).sendKeys(textNaVyplneniPole);
     }
-//
-//    public static void zavreniVyskakovacihoOkna(WebDriver prohlizec, String toast) {
-//        prohlizec.findElement(By.className(toast)).click();
-//    }
 
 }
