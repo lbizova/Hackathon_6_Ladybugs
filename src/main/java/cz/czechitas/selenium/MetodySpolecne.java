18b4598fa407f7b89692706a13fa0b356e5526c3
Lucka Bízová <bizova.lucie@gmail.com>
2021-11-20 13:05:45 +0100

    2 testy hotové (log in, log out), 3. rozpracovaný (kontrola odkazů)

--- src/main/java/cz/czechitas/selenium/MetodySpolecne.java
+++ src/main/java/cz/czechitas/selenium/MetodySpolecne.java
@@ -6,23 +6,30 @@ import org.openqa.selenium.WebDriver;
 public class MetodySpolecne {
 
     public static void nacteniUvodniStranky(WebDriver prohlizec) {
-        prohlizec.navigate().to("https://cz-test-jedna.herokuapp.com/");
+        prohlizec.navigate().to("http://czechitas-datestovani-hackathon.cz/en/");
     }
 
-    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHref(WebDriver prohlizec, String adresa) {
-        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\"]")).click();
+        public static void klikNaTlacitkoDleClass(WebDriver prohlizec, String jmenoTridy) {
+        prohlizec.findElement(By.className(jmenoTridy)).click();
+    }
+        public static void klikNaTlacitkoDleTextu(WebDriver prohlizec, String text) {
+        prohlizec.findElement(By.xpath("//*[text()=\"" + text + "\"]")).click();
     }
 
-    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHrefACastiClass(WebDriver prohlizec, String adresa, String classCast) {
-        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\" and contains (@class, '" + classCast + "')]")).click();
-    }
-
+//    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHref(WebDriver prohlizec, String adresa) {
+//        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\"]")).click();
+//    }
+//
+//    public static void klikNaTlacitkoOdkazujiciNaJinouStrankuDleHrefACastiClass(WebDriver prohlizec, String adresa, String classCast) {
+//        prohlizec.findElement(By.xpath("//a[@href=\"" + adresa + "\" and contains (@class, '" + classCast + "')]")).click();
+//    }
+//
     public static void vyplneniPoleSId(WebDriver prohlizec, String id, String textNaVyplneniPole) {
         prohlizec.findElement(By.id(id)).sendKeys(textNaVyplneniPole);
     }
-
-    public static void zavreniVyskakovacihoOkna(WebDriver prohlizec, String toast) {
-        prohlizec.findElement(By.className(toast)).click();
-    }
+//
+//    public static void zavreniVyskakovacihoOkna(WebDriver prohlizec, String toast) {
+//        prohlizec.findElement(By.className(toast)).click();
+//    }
 
 }
